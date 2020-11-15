8955180d64cf8af145762a86aeadf0cb1e0dcc9b
humayl96 <74428057+humayl96@users.noreply.github.com>
2020-11-15 18:14:16 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-runuser -l  bitnami -c 'cd /opt/bitnami/apache/htdocs/cert-master/ && npm install'
-runuser -l  bitnami -c 'cd /opt/bitnami/apache/htdocs/cert-master/ && composer install'
-cp /home/bitnami/.env /opt/bitnami/apache/htdocs/cert-master/.env
 sudo chown -R bitnami:bitnami /opt/bitnami/apache/htdocs/cert-master/
 sudo chown -R daemon:daemon /opt/bitnami/apache/htdocs/cert-master/storage/
+runuser -l  bitnami -c 'cd /opt/bitnami/apache/htdocs/cert-master/ && npm install'
+runuser -l  bitnami -c 'cd /opt/bitnami/apache/htdocs/cert-master/ && composer install'
+runuser -l  bitnami -c 'cp /home/bitnami/.env /opt/bitnami/apache/htdocs/cert-master/.env'
+
